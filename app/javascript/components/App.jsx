00f10328357318d18cc1b479bc2c1d4fa6d280a5
GeekHarp <ngbhandy@gmail.com>
2019-05-26 16:09:43 +0800

    Layout for Product's Component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import Product from './product/product'
-import Checkout from './checkout/checkout'
+import Product from "./product/product";
+import Checkout from "./checkout/checkout";
 
 export default class App extends React.Component {
-
-	render() {
-		return (
-			<div>
-				<Switch>
-					<Route exact path="/" component={Product} />
-					<Route exact path="/checkout" component={Checkout} />
-				</Switch>
-			</div>
-			);
-	}
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Product} />
+                    <Route exact path="/checkout" component={Checkout} />
+                </Switch>
+            </div>
+        );
+    }
 }
