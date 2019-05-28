1fe7184eb8cc6879f0562f23268a2ddbfb1fa07a
Chang <ainurlindale@gmail.com>
2019-05-28 18:13:34 +0800

    fixed bug on arranging

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,8 +4,7 @@ class CustomersController < ApplicationController
   # GET /customers
   # GET /customers.json
   def index
-    @customers = Customer.all
-
+    @customers = Customer.all.order("created_at ASC")
 
   end
 
