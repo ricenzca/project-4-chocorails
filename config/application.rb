721a401be57cb612a13e46152e2dfdc5bcab7713
ricenzca <ricenzca@hotmail.com>
2019-05-30 14:43:50 +0800

    edited appl.rb

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Chocorails
 
  config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'localhost:3035'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
